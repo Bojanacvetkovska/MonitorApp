91f7dbd836b94350bd298247c4a260d3600f5b19
Bojanacvetkovska <71123066+Bojanacvetkovska@users.noreply.github.com>
2022-09-04 15:25:58 +0200

    Initial commit

---
+++ src/test/java/at/bojana/monitor/MonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.bojana.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
